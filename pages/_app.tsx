dc0c9cc99cc86732931dc7fe75ba0f4f65cf0491
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2023-02-10 10:08:04 +0530

    project init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Main Project</title>
+      </Head>
+      <MantineProvider
+        theme={{
+          // Override any other properties from default theme
+          fontFamily: "DM Sans, Open Sans, sans serif",
+          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+        }}
+      >
+        <ModalsProvider labels={{ confirm: "Submit", cancel: "Cancel" }}>
+          <Component {...pageProps} />
+        </ModalsProvider>
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
